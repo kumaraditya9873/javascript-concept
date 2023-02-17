ad2476fddb5c31e8f487d67cdcf25ceff6fb458e
ADITYA <adityakumar9873@gmail.com>
2023-02-17 11:40:41 +0530

    value updated

---
+++ implicit type conversion/ip.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+// adding two numbers
+let number1 = 12;
+let number2 = 23;
+
+let results = number1 + number2;
+console.log(results);
+
+// here we got the results ,but if the numbers were in string the numbers will be concatenat
+
+let no1 = "55";
+let no2 = "22";
+let sum = no1 + no2;
+console.log(sum);
+// hence the results were concatenated;
+
+/* or even if one of them number will be string and others will be in number the results will be get as string
+ */
