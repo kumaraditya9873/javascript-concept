cbb11f8b39f72e93b22ca36ec5e4458a6e2eaf31
ADITYA <adityakumar9873@gmail.com>
2023-02-17 23:17:50 +0530

    switch case updated

---
+++ switch statement/switch.js
@@ -0,0 +1,46 @@
+// switch statement
+
+const dice = 3;
+switch (dice) {
+  case 1:
+    console.log("you got one");
+    break;
+  case 2:
+    console.log("you got two");
+    break;
+  case 3:
+    console.log("you got three");
+    break;
+  default:
+    console.log("you did not roll the dice");
+}
+// here you can add many cases;;;
+// next example
+
+var day = 4;
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("thrusday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("the day is not in the calender");
+}
