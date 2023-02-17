fbafb94246a4e3e7e9d2c17c4acfb75dae656509
ADITYA <adityakumar9873@gmail.com>
2023-02-17 11:19:51 +0530

    new file added

---
+++ New folder/file.js
@@ -0,0 +1,26 @@
+//  string properties and methods
+
+let name1 = "Aditya Kumar 123";
+let result = name1.length;
+// here the blank spaces will aslo gonna count
+
+console.log(result);
+// result will be 16
+
+// lowercase,uppercase
+
+let name2 = "Ajeet";
+console.log(name2.toLowerCase());
+console.log(name2.toUpperCase());
+
+// to get positon of the alphabhet in a string
+
+let name3 = " peter";
+console.log(name3.charAt(name3.length - 1));
+
+console.log(name3.charAt(2));
+console.log(name3.indexOf("e"));
+// trim the string
+
+console.log(name3.trim());
+console.log(name3.startsWith("peter"));
