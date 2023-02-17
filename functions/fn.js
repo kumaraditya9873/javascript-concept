6262b01cdf0b13a254e49a6b974a213fa69f3d62
ADITYA <adityakumar9873@gmail.com>
2023-02-17 16:13:39 +0530

    function updated

---
+++ functions/fn.js
@@ -0,0 +1,19 @@
+function hello() {
+  console.log("hello there");
+  console.log("hello world");
+  console.log("hello neon");
+}
+
+hello();
+
+// adding two numbers using functions
+// here x,y are the parameters
+function add(x, y) {
+  let sum = x + y;
+
+  return sum; // return//
+}
+
+let results = add(5, 10);
+console.log(results); // result will be 15;
+// here 5,10 were arguments
