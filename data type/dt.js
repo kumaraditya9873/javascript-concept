c2d62db3f74200fdfc9ba99ef7b4f9a2416eab4e
ADITYA <adityakumar9873@gmail.com>
2023-02-17 12:36:20 +0530

    data type file added

---
+++ data type/dt.js
@@ -0,0 +1,15 @@
+// typeof value or variables
+const text = true; // boolean
+const text2 = "bheem"; //string
+
+const text3 = 12; // number
+
+console.log(typeof text);
+console.log(typeof text2);
+console.log(typeof text3);
+
+const result = null; // object
+console.log(typeof result);
+
+let name;
+console.log(name);
