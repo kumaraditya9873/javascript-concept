78ba61607602a5675f10e82291b53a4b13697fde
ADITYA <adityakumar9873@gmail.com>
2023-02-20 20:49:16 +0530

    ternary operator

---
+++ ternary operator/tn.js
@@ -0,0 +1,23 @@
+const value = 2 > 1;
+if (value) {
+  console.log("the value is true");
+} else {
+  console.log("the value is false");
+}
+
+// using ternary operator
+
+value ? console.log("the value is true") : console.log("the value is false");
+
+let a = 2;
+let b = 4;
+
+if (b < a) {
+  console.log("the value is true");
+} else {
+  console.log("the value is false");
+}
+
+// using ternary operator
+
+b < a ? console.log("the value is true") : console.log("the value is false");
