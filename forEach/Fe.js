b5558b85c51f23997e23e4eaa0043ef22d0f12f1
ADITYA <adityakumar9873@gmail.com>
2023-02-21 12:19:47 +0530

    forEach Updated

---
+++ forEach/Fe.js
@@ -0,0 +1,17 @@
+let details = [
+  { name: "aditya", age: 26, skills: "javaScript" },
+  { name: "Raj", age: 21, skills: "java" },
+  { name: "Abhi", age: 19, skills: "Php" },
+];
+
+// function ShowDetails(details) {
+//   console.log(details);
+// }
+
+// details.forEach(ShowDetails);
+
+// or
+
+details.forEach(function (showDetails) {
+  console.log(showDetails);
+});
